f387c65cb3c47f1c8c34ee7a33e854514108b884
Keefer Bibby <keefer.g.bibby@gmail.com>
2018-04-08 13:59:26 -0400

    term input using formfor

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,24 @@
 module CoursesHelper
+  def getYearsArray
+    years = [[(Date.today - 5.years).year, (Date.today - 5.years).year],
+            [(Date.today - 4.years).year, (Date.today - 4.years).year],
+            [(Date.today - 3.years).year, (Date.today - 3.years).year],
+            [(Date.today - 2.years).year, (Date.today - 2.years).year],
+            [(Date.today - 1.years).year, (Date.today - 1.years).year],
+            [Date.today.year, Date.today.year],
+            [(Date.today + 1.years).year, (Date.today + 1.years).year],
+            [(Date.today + 2.years).year, (Date.today + 2.years).year],
+            [(Date.today + 3.years).year, (Date.today + 3.years).year],
+            [(Date.today + 4.years).year, (Date.today + 4.years).year],
+            [(Date.today + 5.years).year, (Date.today + 5.years).year],
+            [(Date.today + 6.years).year, (Date.today + 6.years).year]]
+  end
+
+  def getTermsArray
+    terms = [["Fall", "Fall"],
+            ["Winter", "Winter"],
+            ["Spring", "Spring"],
+            ["Summer", "Summer"]]
+  end
+
 end
