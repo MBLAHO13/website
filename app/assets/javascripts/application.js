4da7e484b21047f802a4c19e134f0de40f8e547a
Keefer Bibby <keefer.g.bibby@gmail.com>
2018-05-16 13:43:50 -0400

    basic input and course info display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,6 +31,7 @@ $('#courseInfoModal').on('show.bs.modal', function (event) {
   modal.find('.modal-body input').val(recipient)
 })
 
+// Dynamic term input rows
 $(function()
 {
   $(document).on('click', '.btn-add#add-term', function(e)
@@ -67,3 +68,93 @@ $(function()
     return false;
   })
 });
+
+$(function()
+{
+  let course = $(this).val();
+  $(document).on('click', '.course-more-info', function(e)
+  {
+    e.preventDefault();
+    let coursename = this.text;
+    coursename = coursename.replace(" ", "/");
+    requestUrl = "http://advisor-ai.tech:8000/api/courses/" + coursename;
+
+    $.ajax({
+      url: requestUrl,
+      type: 'GET',
+
+      success: function(response) {
+        let studentLevel = ["Freshman", "Sophomore", "Pre-Junior", "Junior", "Senior"];
+        let minLevel = studentLevel[response[0].min_class];
+        let maxLevel = studentLevel[response[0].max_class];
+        let restrictions = "Available to " + minLevel + " level through " + maxLevel + " level students.";
+        $('#modal-course').html(response[0].dept + " " + response[0].code);
+        $('#modal-title').html(response[0].title);
+        $('#modal-description').html(response[0].description);
+        $('#modal-restrictions').html(restrictions);
+        // $('#modal-course').html(`${response.current_observation.feelslike_c}`);
+        // $('#modal-title').html(`${response.current_observation.feelslike_f}`);
+        // $('#modal-description').html(`${response.current_observation.dewpoint_c}`);
+        // $('#modal-prereq-of').html(`${response.current_observation.dewpoint_f}`);
+        // $('#modal-prereq-for').html(`${response.current_observation.UV}`);
+      },
+      error: function() {
+
+      }
+    });
+  })
+});
+
+// $( function() {
+//   e.preventDefault();
+//   requestUrl = "http://advisor-ai.tech:8000/api/courses/all";
+//
+//   $.ajax({
+//     url: requestUrl,
+//     type: 'GET',
+//
+//     success: function(response) {
+//       console.log(response);
+//       $('#modal-course').html(coursename);
+//       // $('#modal-title').html(coursename);
+//       // $('#modal-description').html(coursename);
+//       // $('#modal-prereq-of').html(coursename);
+//       // $('#modal-prereq-for').html(coursename);
+//       // $('#modal-course').html(`${response.current_observation.feelslike_c}`);
+//       // $('#modal-title').html(`${response.current_observation.feelslike_f}`);
+//       // $('#modal-description').html(`${response.current_observation.dewpoint_c}`);
+//       // $('#modal-prereq-of').html(`${response.current_observation.dewpoint_f}`);
+//       // $('#modal-prereq-for').html(`${response.current_observation.UV}`);
+//     },
+//     error: function() {
+//
+//     }
+//   });
+//     var availableTags = [
+//       "ActionScript",
+//       "AppleScript",
+//       "Asp",
+//       "BASIC",
+//       "C",
+//       "C++",
+//       "Clojure",
+//       "COBOL",
+//       "ColdFusion",
+//       "Erlang",
+//       "Fortran",
+//       "Groovy",
+//       "Haskell",
+//       "Java",
+//       "JavaScript",
+//       "Lisp",
+//       "Perl",
+//       "PHP",
+//       "Python",
+//       "Ruby",
+//       "Scala",
+//       "Scheme"
+//     ];
+//     $( "#course-input-field" ).autocomplete({
+//       source: availableTags
+//     });
+//   } );
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'view/degree'
+  get 'view/degree'
 
   get 'view/tree'
 
